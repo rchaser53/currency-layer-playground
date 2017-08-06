4cb31dbf6cd79b500ae05ae1f2c3383ab9036734
rchaser53 <tayoshizawa29@gmail.com>
2017-08-06 23:20:28 +0900

    try to use currency layer with maxage

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const http = require('http')
+const fetch = require('node-fetch');
 const express = require('express')
 const app = express()
 
+const apiKey = 'nya-n'
+
 app.get('/', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.send('{"key": "value"}')
+  res.header('Cache-Control', 'max-age=60')
+  fetch(`http://apilayer.net/api/live?access_key=${apiKey}`)
+  .then((ret) => {
+    return ret.json()
+  })
+  .then((ret) => {
+    res.send(JSON.stringify(ret))
+  })
 });
 
 http.createServer(app)
