d5406295e606e411f11ca7b3f80e0414e09bc1c2
rchaser53 <tayoshizawa29@gmail.com>
2017-08-15 23:23:41 +0900

    try to register service worker

--- src/script1.js
+++ src/script1.js
@@ -1,13 +1,9 @@
-      window.onload = () => {
-        document.querySelector('#sendRequest').addEventListener('click', (event) => {
-          fetch('http://localhost:2000/hoge', {
-            mode: 'cors'
-          })
-          .then((ret) => {
-              return ret.json()
-          })
-          .then((ret) => {
-            console.log(ret)
-          })
-        })
-      }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./src/serviceWorker.js').then(function(registration) {
+    // 登録成功
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // 登録失敗 :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
